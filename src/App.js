806f6ce0fce2f002a771e5379479d61097f2171e
Talbekk <mt.campbell@outlook.com>
2021-02-16 17:48:36 +0000

    creates header container

--- src/App.js
+++ src/App.js
@@ -1,30 +1,10 @@
 import React from "react";
-import logo from "./img/logo.png";
-import "./App.css";
+import HeaderContainer from "./containers/HeaderContainer";
 
 function App() {
   return (
     <div className="App">
-      <header className="header">
-        <nav className="header-nav">
-          <img className="logo" src={logo} alt="Travelling Teacher Logo"></img>
-          <ul className="main-nav">
-              <li><a className="main-nav-item" href="#about-me">About</a></li>
-              <li><a className="main-nav-item" href="#services">Services</a></li>
-              <li><a className="main-nav-item" href="#packages">Packages</a></li>
-              <li><a className="main-nav-item" href="#enquiries">Enquiries</a></li>
-          </ul>
-        </nav>
-        <div className="header-hero">
-          <h1 className="heading-primary">
-            <div className="heading-primary-main">
-            <span className="heading-primary-main-left">Michael</span>
-            <span className="heading-primary-main-right">Campbell</span>
-            </div>
-            <span className="heading-primary-sub">The Travelling Teacher</span>
-          </h1>
-        </div>
-      </header>
+      <HeaderContainer/>
     </div>
   );
 }
---
+++ src/containers/HeaderContainer.js
@@ -0,0 +1,30 @@
+import React from "react";
+import logo from "../img/logo.png";
+import "../App.css";
+
+function HeaderContainer() {
+  return (
+      <header className="header">
+        <nav className="header-nav">
+          <img className="logo" src={logo} alt="Travelling Teacher Logo"></img>
+          <ul className="main-nav">
+              <li><a className="main-nav-item" href="#about-me">About</a></li>
+              <li><a className="main-nav-item" href="#services">Services</a></li>
+              <li><a className="main-nav-item" href="#packages">Packages</a></li>
+              <li><a className="main-nav-item" href="#enquiries">Enquiries</a></li>
+          </ul>
+        </nav>
+        <div className="header-hero">
+          <h1 className="heading-primary">
+            <div className="heading-primary-main">
+            <span className="heading-primary-main-left">Michael</span>
+            <span className="heading-primary-main-right">Campbell</span>
+            </div>
+            <span className="heading-primary-sub">The Travelling Teacher</span>
+          </h1>
+        </div>
+      </header>
+  );
+}
+
+export default HeaderContainer;
