94f6f12b7ba717ca93fe59c10c785faf8dcb4658
Talbekk <mt.campbell@outlook.com>
2021-02-16 17:56:39 +0000

    creates hero component

---
+++ src/components/Hero.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../App.css";
+
+function Hero() {
+  return (
+        <h1 className="heading-primary">
+            <div className="heading-primary-main">
+            <span className="heading-primary-main-left">Michael</span>
+            <span className="heading-primary-main-right">Campbell</span>
+            </div>
+            <span className="heading-primary-sub">The Travelling Teacher</span>
+        </h1>
+  );
+}
+
+export default Hero;
--- src/containers/HeaderContainer.js
+++ src/containers/HeaderContainer.js
@@ -1,21 +1,14 @@
 import React from "react";
 import logo from "../img/logo.png";
 import Navigation from "../components/Navigation";
+import Hero from "../components/Hero";
 import "../App.css";
 
 function HeaderContainer() {
   return (
       <header className="header">
-          <Navigation/>
-        <div className="header-hero">
-          <h1 className="heading-primary">
-            <div className="heading-primary-main">
-            <span className="heading-primary-main-left">Michael</span>
-            <span className="heading-primary-main-right">Campbell</span>
-            </div>
-            <span className="heading-primary-sub">The Travelling Teacher</span>
-          </h1>
-        </div>
+        <Navigation/>
+        <Hero/>
       </header>
   );
 }
