f3e963ea8b6770886a9b3e547a312b482e3b6392
Talbekk <mt.campbell@outlook.com>
2021-02-16 15:04:15 +0000

    adds basic nav styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from "react";
+import logo from "./img/logo.png";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="header">
-        <nav class="header-nav"></nav>
-        <div class="header-hero"></div>
+        <nav className="header-nav">
+          <img className="logo" src={logo} alt="Travelling Teacher Logo"></img>
+          <ul className="main-nav">
+              <li><a className="main-nav-item" href="#about-me">About</a></li>
+              <li><a className="main-nav-item" href="#services">Services</a></li>
+              <li><a className="main-nav-item" href="#packages">Packages</a></li>
+              <li><a className="main-nav-item" href="#enquiries">Enquiries</a></li>
+          </ul>
+        </nav>
+        <div className="header-hero"></div>
       </header>
     </div>
   );
