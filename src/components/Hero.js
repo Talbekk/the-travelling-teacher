ef85b7911653727bc74918683e36421a00dcd8a3
Talbekk <mt.campbell@outlook.com>
2021-02-17 22:08:40 +0000

    refactors navigation styling into sass

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,11 +4,11 @@ import "../App.css";
 function Hero() {
   return (
         <h1 className="heading-primary">
-            <div className="heading-primary-main">
-            <span className="heading-primary-main-left">Michael</span>
-            <span className="heading-primary-main-right">Campbell</span>
+            <div className="heading-primary--main">
+            <span className="heading-primary--main-left">Michael</span>
+            <span className="heading-primary--main-right">Campbell</span>
             </div>
-            <span className="heading-primary-sub">The Travelling Teacher</span>
+            <span className="heading-primary--sub">The Travelling Teacher</span>
         </h1>
   );
 }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,13 +4,13 @@ import "../App.css";
 
 function Navigation() {
   return (
-        <nav className="header-nav">
-          <img className="logo" src={logo} alt="Travelling Teacher Logo"></img>
-          <ul className="main-nav">
-              <li><a className="main-nav-item" href="#about-me">About</a></li>
-              <li><a className="main-nav-item" href="#services">Services</a></li>
-              <li><a className="main-nav-item" href="#packages">Packages</a></li>
-              <li><a className="main-nav-item" href="#enquiries">Enquiries</a></li>
+        <nav className="header__nav">
+          <img className="header__logo" src={logo} alt="Travelling Teacher Logo"></img>
+          <ul className="header__main-nav">
+              <li><a className="header__main-nav--item" href="#about-me">About</a></li>
+              <li><a className="header__main-nav--item" href="#services">Services</a></li>
+              <li><a className="header__main-nav--item" href="#packages">Packages</a></li>
+              <li><a className="header__main-nav--item" href="#enquiries">Enquiries</a></li>
           </ul>
         </nav>
   );
