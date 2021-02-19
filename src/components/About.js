1d321e26a2242f337bb0796c060e89bb5dab5495
Talbekk <mt.campbell@outlook.com>
2021-02-19 15:36:28 +0000

    adds in info section for about

--- src/components/About.js
+++ src/components/About.js
@@ -3,16 +3,21 @@ import "../App.css";
 
 function About() {
   return (
-    <section className="section-about">
-        <h2 className="heading-secondary">
+    <section className="section-about" id="section-about">
+        <h2 className="heading-secondary u-margin-bottom-8">
             About Me
         </h2>
-        <div className="about__content">
-            <div className="about__content--text">
-
+        <div className="about-content">
+            <div className="about-content__text">
+                <p className="paragraph">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+                <p className="paragraph">
+                    Faucibus interdum posuere lorem ipsum dolor sit. Sagittis eu volutpat odio facilisis mauris sit amet massa. Quam vulputate dignissim suspendisse in. Faucibus pulvinar elementum integer enim neque volutpat. Egestas sed sed risus pretium quam vulputate dignissim. Et tortor at risus viverra adipiscing. Pulvinar mattis nunc sed blandit libero.
+                </p>
             </div>
-            <div className="about__content--images">
-
+            <div className="about-content__images">
+                This is for the images.
             </div>
         </div>
     </section>
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,7 +7,7 @@ function Navigation() {
         <nav className="header__nav">
           <img className="header__logo" src={logo} alt="Travelling Teacher Logo"></img>
           <ul className="header__main-nav">
-              <li><a className="header__main-nav--item" href="#about-me">About</a></li>
+              <li><a className="header__main-nav--item" href="#section-about">About</a></li>
               <li><a className="header__main-nav--item" href="#services">Services</a></li>
               <li><a className="header__main-nav--item" href="#packages">Packages</a></li>
               <li><a className="header__main-nav--item" href="#enquiries">Enquiries</a></li>
