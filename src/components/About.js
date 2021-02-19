d093406e19381d65d379005d65128145e838dee6
Talbekk <mt.campbell@outlook.com>
2021-02-19 16:20:20 +0000

    adds react scroll package

--- src/components/About.js
+++ src/components/About.js
@@ -11,7 +11,7 @@ function About() {
             About Me
         </h2>
         <div className="about-content">
-            <div className="about-content__text">
+            <div className="about-content__text u-margin-left-5">
                 <p className="paragraph">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </p>
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,6 @@
 import React from "react";
 import logo from "../img/logo.png";
+import { Link } from "react-scroll";
 import "../App.css";
 
 function Navigation() {
@@ -7,10 +8,10 @@ function Navigation() {
         <nav className="header__nav">
           <img className="header__logo" src={logo} alt="Travelling Teacher Logo"></img>
           <ul className="header__main-nav">
-              <li><a className="header__main-nav--item" href="#section-about">About</a></li>
-              <li><a className="header__main-nav--item" href="#services">Services</a></li>
-              <li><a className="header__main-nav--item" href="#packages">Packages</a></li>
-              <li><a className="header__main-nav--item" href="#enquiries">Enquiries</a></li>
+              <li><Link spy={true} smooth={true} offset={-70} duration={600} className="header__main-nav--item" to="section-about">About</Link></li>
+              <li><Link spy={true} smooth={true} offset={-70} duration={600} className="header__main-nav--item" to="services">Services</Link></li>
+              <li><Link spy={true} smooth={true} offset={-70} duration={600} className="header__main-nav--item" href="packages">Packages</Link></li>
+              <li><Link spy={true} smooth={true} offset={-70} duration={600} className="header__main-nav--item" href="enquiries">Enquiries</Link></li>
           </ul>
         </nav>
   );
