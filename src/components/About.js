cbe0f073050e899df1bd7d7fc35f180fc2e54c83
Talbekk <mt.campbell@outlook.com>
2021-02-19 14:30:02 +0000

    adds new components for about me

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../App.css";
+
+function About() {
+  return (
+    <section className="section-about">
+        <h2 className="heading-secondary">
+            About Me
+        </h2>
+
+    </section>
+  );
+}
+
+export default About;
---
+++ src/containers/MainContainer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import About from "../components/About";
+import "../App.css";
+
+function MainContainer() {
+  return (
+   <main>
+       <About/>
+   </main>
+  );
+}
+
+export default MainContainer;
