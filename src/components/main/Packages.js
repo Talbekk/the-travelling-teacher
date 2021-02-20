f9414123155279cbb126851ce7a119c202b526bc
Talbekk <mt.campbell@outlook.com>
2021-02-20 14:26:00 +0000

    adds basic packages content

--- src/components/main/Packages.js
+++ src/components/main/Packages.js
@@ -8,14 +8,14 @@ function Packages() {
             Packages
         </h2>
         <div className="packages-content">
-            <div class="card">
-                <div class="card__header">
-                    <h4 class="card__header--heading">Test Header</h4>
+            <div className="card">
+                <div className="card__header">
+                    <h4 className="card__header--heading">Test Header</h4>
                     <p className="card__heading--price">£3.00 per class</p>
                     <p className="card__heading--description">Only £3 per class</p>
                 </div>
-                <div class="card__details">
-                    <ul>
+                <div className="card__details">
+                    <ul className="card__details--list">
                         <li>3 day tour</li>
                         <li>Up to 30 people</li>
                         <li>2 tour guides</li>
@@ -23,16 +23,16 @@ function Packages() {
                         <li>Difficulty: easy</li>
                     </ul>
                 </div>
-                <button>Enquire Now</button>
+                <button className="card__btn">Enquire Now</button>
             </div>
-            <div class="card">
-                <div class="card__header">
-                    <h4 class="card__header--heading">Test Header</h4>
+            <div className="card">
+                <div className="card__header">
+                    <h4 className="card__header--heading">Test Header</h4>
                     <p className="card__heading--price">£3.00 per class</p>
                     <p className="card__heading--description">Only £3 per class</p>
                 </div>
-                <div class="card__details">
-                    <ul>
+                <div className="card__details">
+                    <ul className="card__details--list">
                         <li>3 day tour</li>
                         <li>Up to 30 people</li>
                         <li>2 tour guides</li>
@@ -40,16 +40,16 @@ function Packages() {
                         <li>Difficulty: easy</li>
                     </ul>
                 </div>
-                <button>Enquire Now</button>
+                <button className="card__btn">Enquire Now</button>
             </div>
-            <div class="card">
-                <div class="card__header">
-                    <h4 class="card__header--heading">Test Header</h4>
+            <div className="card">
+                <div className="card__header">
+                    <h4 className="card__header--heading">Test Header</h4>
                     <p className="card__heading--price">£3.00 per class</p>
                     <p className="card__heading--description">Only £3 per class</p>
                 </div>
-                <div class="card__details">
-                    <ul>
+                <div className="card__details">
+                    <ul className="card__details--list">
                         <li>3 day tour</li>
                         <li>Up to 30 people</li>
                         <li>2 tour guides</li>
@@ -57,7 +57,7 @@ function Packages() {
                         <li>Difficulty: easy</li>
                     </ul>
                 </div>
-                <button>Enquire Now</button>
+                <button className="card__btn">Enquire Now</button>
             </div>
         </div>
     </section>
