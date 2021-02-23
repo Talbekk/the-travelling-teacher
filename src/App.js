eaf78c0cdbf9beb7b9bc251ba6fecfe7c74256e6
Talbekk <mt.campbell@outlook.com>
2021-02-23 17:20:52 +0000

    links up basic nav

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import HeaderContainer from "./containers/HeaderContainer";
 import MainContainer from "./containers/MainContainer";
+import FooterContainer from "./components/footer/Footer";
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <div className="app">
       <HeaderContainer/>
       <MainContainer/>
+      <FooterContainer/>
     </div>
   );
 }
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../../App.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+        <div className="footer__about-dev">
+            Test
+        </div>
+        <div className="footer__social-list">
+            Test
+        </div>
+    </footer>
+  )
+}
+
+export default Footer;
