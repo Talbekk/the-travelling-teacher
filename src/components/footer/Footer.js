84b0c230810dcf5d3987df63d87953a0125b0a30
Talbekk <mt.campbell@outlook.com>
2021-02-24 21:11:17 +0000

    adds the icons for social links in footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,17 +1,22 @@
 import React from "react";
 import "../../App.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faRebel } from '@fortawesome/free-brands-svg-icons';
 
 function Footer() {
   return (
     <footer className="footer">
         <p className="footer__about-dev">
-        Copyright &copy; 2021 by Michael Campbell @ talbekk_dev. Check out my socials to the right to see nore.
+          Copyright &copy; 2021 by Michael Campbell @ talbekk_dev
         </p>
             <ul className="footer__list">
-                <li className="footer__item"><a href="#" className="footer__link">Github</a></li>
-                <li className="footer__item"><a href="#" className="footer__link">LinkedIn</a></li>
-                <li className="footer__item"><a href="#" className="footer__link">Twitter</a></li>
-                <li className="footer__item"><a href="#" className="footer__link">Portfolio</a></li>
+                <li className="footer__item"><a href="#" className="footer__link"><FontAwesomeIcon icon={faGithub}/></a></li>
+                <li className="footer__item"><a href="#" className="footer__link"><FontAwesomeIcon icon={faLinkedin}/></a></li>
+                <li className="footer__item"><a href="#" className="footer__link"><FontAwesomeIcon icon={faTwitter}/></a></li>
+                <li className="footer__item"><a href="#" className="footer__link"><FontAwesomeIcon icon={faRebel}/></a></li>
             </ul>
     </footer>
   )
--- src/components/main/Enquiries.js
+++ src/components/main/Enquiries.js
@@ -58,7 +58,7 @@ function Enquiries() {
                         <label htmlFor="message" className="form__label">Message</label>
                         <textarea className="form_textarea" id="message" cols="50" rows="5" placeholder="Please add any other comments or questions here..." className="form__textarea" required/>
                     </div>
-                    <button className="btn btn--blue align-left">Send Form</button>
+                    <button className="btn btn--blue btn--form align-left" type="submit">Send Form</button>
                 </form>
             </div>
         </div>
