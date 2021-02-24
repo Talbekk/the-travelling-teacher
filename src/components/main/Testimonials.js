9c6f315f6fe6d87648842a372e5b38f425eb6f38
Talbekk <mt.campbell@outlook.com>
2021-02-24 21:19:36 +0000

    adds stock images for testimonials

--- src/components/main/Testimonials.js
+++ src/components/main/Testimonials.js
@@ -1,5 +1,6 @@
 import React from "react";
-import logo from "../../img/logo.png";
+import leo from "../../img/profile-1.jpg";
+import kim from "../../img/profile-2.jpg";
 import "../../App.css";
 
 function Testimonials() {
@@ -13,7 +14,7 @@ function Testimonials() {
             &ldquo;Maecenas eget urna mi. Integer sed sem et urna laoreet bibendum vel ut mauris. In hac habitasse platea dictumst. Integer pulvinar lacinia justo, et pulvinar libero consectetur eget. Nulla facilisi. Praesent pretium est ac tortor lobortis venenatis. Curabitur a accumsan est. Vivamus et mauris sem. Fusce molestie enim at condimentum iaculis.&rdquo;
             </blockquote>
             <figcaption className="review__user">
-                <img src={logo} alt="User 1" className="review__photo"/>
+                <img src={leo} alt="User 1" className="review__photo"/>
                 <div className="review__user-box">
                     <p className="review__user-name">Leo Kim</p>
                     <p className="review__user-date">Jun. 23rd 2018</p>
@@ -26,7 +27,7 @@ function Testimonials() {
             &ldquo;Aenean urna lorem, porttitor fringilla suscipit sit amet, ornare at augue. Proin aliquet nec velit vel consectetur. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Phasellus accumsan ultrices est id vulputate. Nulla pulvinar malesuada mi non ullamcorper.&rdquo;
             </blockquote>
             <figcaption className="review__user">
-                <img src={logo} alt="User 1" className="review__photo"/>
+                <img src={kim} alt="User 1" className="review__photo"/>
                 <div className="review__user-box">
                     <p className="review__user-name">Kim Sukyoung</p>
                     <p className="review__user-date">Mar. 4th 2018</p>
