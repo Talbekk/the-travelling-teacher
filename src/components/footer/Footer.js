30c27840cf30e2b4830f8ac730f3dfb241b4d557
Talbekk <mt.campbell@outlook.com>
2021-02-24 21:24:48 +0000

    updates image size

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -13,10 +13,10 @@ function Footer() {
           Copyright &copy; 2021 by Michael Campbell @ talbekk_dev
         </p>
             <ul className="footer__list">
-                <li className="footer__item"><a href="#" className="footer__link"><FontAwesomeIcon icon={faGithub}/></a></li>
-                <li className="footer__item"><a href="#" className="footer__link"><FontAwesomeIcon icon={faLinkedin}/></a></li>
-                <li className="footer__item"><a href="#" className="footer__link"><FontAwesomeIcon icon={faTwitter}/></a></li>
-                <li className="footer__item"><a href="#" className="footer__link"><FontAwesomeIcon icon={faRebel}/></a></li>
+                <li className="footer__item"><a href="https://github.com/Talbekk" target="_blank" rel="noreferrer" className="footer__link"><FontAwesomeIcon icon={faGithub}/></a></li>
+                <li className="footer__item"><a href="https://www.linkedin.com/in/mt-campbell/" target="_blank" rel="noreferrer" className="footer__link"><FontAwesomeIcon icon={faLinkedin}/></a></li>
+                <li className="footer__item"><a href="https://twitter.com/?lang=en-gb" target="_blank" rel="noreferrer" className="footer__link"><FontAwesomeIcon icon={faTwitter}/></a></li>
+                <li className="footer__item"><a href="https://starwars.fandom.com/wiki/Alliance_to_Restore_the_Republic" target="_blank" rel="noreferrer" className="footer__link"><FontAwesomeIcon icon={faRebel}/></a></li>
             </ul>
     </footer>
   )
