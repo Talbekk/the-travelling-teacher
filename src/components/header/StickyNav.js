61bc9d79811de8f214bc3a95de4f8f0ac9a434cd
Talbekk <mt.campbell@outlook.com>
2021-02-25 21:38:42 +0000

    fixes sticky nav bar bug

--- src/components/header/StickyNav.js
+++ src/components/header/StickyNav.js
@@ -7,12 +7,8 @@ function StickyNav() {
     const [ showStickyNav, setShowStickyNav ] = useState(false);
     const [ ticking, setTicking ] = useState(false);
     const [ scrollPosition, setScrollPosition ] = useState(0);
-
-    // let lastKnownScrollPosition = 0;
-    // let ticking = false;
     
     function doSomething(scrollPos) {
-      // Do something with the scroll position
       if (scrollPos  >= 300) {
        setShowStickyNav(true);
     }
@@ -23,7 +19,6 @@ function StickyNav() {
     
     document.addEventListener('scroll', function(e) {
         setScrollPosition(window.scrollY);
-        // lastKnownScrollPosition = window.scrollY;
     
       if (!ticking) {
         window.requestAnimationFrame(function() {
@@ -35,23 +30,16 @@ function StickyNav() {
       }
     });
 
-    // window.scroll(function(){
-    //     if (window.scrollTop() >= 300) {
-    //         ('nav').addClass('fixed-header');
-    //     }
-    //     else {
-    //         ('nav').removeClass('fixed-header');
-    //     }
-    // });
-
   return (
-        <nav className={(showStickyNav) ? "header__sticky-nav fixed-header" : "header__sticky-nav"}>
+        <nav className={(showStickyNav) ? "show-header" : "hide-header"}>
+            <ul className="header__main-nav">
               <li><Link spy={true} smooth={true} offset={-70} duration={600} className="header__main-nav--item" to="section-about">About</Link></li>
               <li><Link spy={true} smooth={true} offset={-70} duration={600} className="header__main-nav--item" to="section-services">Services</Link></li>
               <li><Link spy={true} smooth={true} offset={-70} duration={600} className="header__main-nav--item" to="section-packages">Packages</Link></li>
               <li><Link spy={true} smooth={true} offset={-70} duration={600} className="header__main-nav--item" to="section-testimonials">Testimonials</Link></li>
               <li><Link spy={true} smooth={true} offset={-70} duration={600} className="header__main-nav--item" to="section-enquiries">Enquiries</Link></li>
-          </nav>
+            </ul>
+        </nav>
         );
 }
 
