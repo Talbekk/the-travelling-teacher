123c74fc866d52462f06975b43042a1b5a6a2b2a
Talbekk <mt.campbell@outlook.com>
2021-02-25 21:09:28 +0000

    sticky nav is working now

--- src/components/header/StickyNav.js
+++ src/components/header/StickyNav.js
@@ -1,29 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-scroll";
 import "../../App.css";
 
 function StickyNav() {
 
-    window.scroll(function(){
-        if (window.scrollTop() >= 300) {
-            ('nav').addClass('fixed-header');
-        }
-        else {
-            ('nav').removeClass('fixed-header');
-        }
+    const [ showStickyNav, setShowStickyNav ] = useState(false);
+    const [ ticking, setTicking ] = useState(false);
+    const [ scrollPosition, setScrollPosition ] = useState(0);
+
+    // let lastKnownScrollPosition = 0;
+    // let ticking = false;
+    
+    function doSomething(scrollPos) {
+      // Do something with the scroll position
+      if (scrollPos  >= 300) {
+       setShowStickyNav(true);
+    }
+    else {
+        setShowStickyNav(false);
+    }
+    }
+    
+    document.addEventListener('scroll', function(e) {
+        setScrollPosition(window.scrollY);
+        // lastKnownScrollPosition = window.scrollY;
+    
+      if (!ticking) {
+        window.requestAnimationFrame(function() {
+        doSomething(scrollPosition);
+        setTicking(false);
+        });
+    
+        setTicking(true);
+      }
     });
 
+    // window.scroll(function(){
+    //     if (window.scrollTop() >= 300) {
+    //         ('nav').addClass('fixed-header');
+    //     }
+    //     else {
+    //         ('nav').removeClass('fixed-header');
+    //     }
+    // });
+
   return (
-        <nav className="header__sticky-nav">
-          <ul className="header__main-nav">
+        <nav className={(showStickyNav) ? "header__sticky-nav fixed-header" : "header__sticky-nav"}>
               <li><Link spy={true} smooth={true} offset={-70} duration={600} className="header__main-nav--item" to="section-about">About</Link></li>
               <li><Link spy={true} smooth={true} offset={-70} duration={600} className="header__main-nav--item" to="section-services">Services</Link></li>
               <li><Link spy={true} smooth={true} offset={-70} duration={600} className="header__main-nav--item" to="section-packages">Packages</Link></li>
               <li><Link spy={true} smooth={true} offset={-70} duration={600} className="header__main-nav--item" to="section-testimonials">Testimonials</Link></li>
               <li><Link spy={true} smooth={true} offset={-70} duration={600} className="header__main-nav--item" to="section-enquiries">Enquiries</Link></li>
-          </ul>
-        </nav>
-  );
+          </nav>
+        );
 }
 
 export default StickyNav;
