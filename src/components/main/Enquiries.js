d1a4341b29f685749e892554318149c9c7a9ac44
Talbekk <mt.campbell@outlook.com>
2021-10-23 10:59:46 +0100

    adds space to placeholder text

--- src/components/main/Enquiries.js
+++ src/components/main/Enquiries.js
@@ -55,7 +55,7 @@ function Enquiries() {
                     </div>
                     <div className="form__group-textarea">
                         <label htmlFor="message" className="form__label">Message</label>
-                        <textarea className="form_textarea" id="message" cols="50" rows="5" placeholder="Please add any other comments or questions here..."/>
+                        <textarea className="form_textarea" id="message" cols="50" rows="5" placeholder=" Please add any other comments or questions here..."/>
                     </div>
                     <button className="btn btn--blue btn--form align-left" type="submit">Send Form</button>
                 </form>
